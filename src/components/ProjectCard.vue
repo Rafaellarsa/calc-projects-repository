d459f80461523d57e14a7c131269e13211c603d7
Rafaella Sampaio <sampaiorafaella@hotmail.com>
2025-11-03 14:53:31 -0500

    removed hours and added authors

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -19,7 +19,7 @@
           <v-col
             ><b>Difficulty:</b> {{ difficultyNames[project.difficulty] }}</v-col
           >
-          <v-col><b>Hours:</b> {{ project.hours }} hours</v-col></v-row
+          <v-col><b>Author:</b> {{ project.author }}</v-col></v-row
         ><v-row no-gutters>
           <v-col
             ><b>Keywords:</b> {{ project.keywords.join(", ") }}</v-col
